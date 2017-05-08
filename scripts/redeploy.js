3f2caff9478718766cfa821fd03dc62a70ae7042
Petter Hareim <petter.hareim@bouvet.no>
2017-05-08 18:43:15 +0200

    update redeploy

--- scripts/redeploy.js
+++ scripts/redeploy.js
@@ -2,14 +2,12 @@ const spawn = require('child_process').spawn;
 var doRedeploy = false;
 module.exports = function (robot) {
   robot.respond(/redeploy bernard!/i, function (res) {
-    res.send('Yessir. Jeg redeployer hvis jeg finner noe nytt. :spock-hand:');
     var pull = spawn('git', ['pull']);
     pull.stdout.on('data', data => {});
 
     pull.stderr.on('data', data => {
       res.send(`Ooops: ${data}`);
     });
-
     pull.on('close', code => {
       if (code === 0)
         res.send(`:thumbsup:`);
