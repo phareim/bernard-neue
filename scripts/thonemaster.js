eda54ddbc4f8a0ff1bfdd5387f72479c0b54cb28
Petter Hareim <petter.hareim@bouvet.no>
2017-09-03 18:36:13 +0200

    status working.

--- scripts/thonemaster.js
+++ scripts/thonemaster.js
@@ -3,9 +3,9 @@
 var thronemaster = require('thronemaster-tools');
 var id = 131993;
 module.exports = function (robot) {
-  robot.respond(/status (.*)/i, function (res) {
+  robot.respond(/status/i, function (res) {
     thronemaster.getLog(id, function (status) {
-      var t = status.pop();
+      var                     t = status.pop();
       res.send('Siste flytt i runde ' + t.turn + ' er: ' + t.player + ' ' + t.logEntry);
     });
   });
