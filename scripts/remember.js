217795f9f27a920c7f9109cb617f4fe35cb3ec99
Petter Hareim <petter.hareim@bouvet.no>
2017-09-04 22:13:47 +0200

    testing out hubot brain.

--- scripts/remember.js
+++ scripts/remember.js
@@ -4,10 +4,16 @@
 module.exports = function (robot) {
   robot.respond(/husk: (.*)/i, function (res) {
     var husk = res.match[1];
+    robot.brain.set('husk', husk);
     res.send('ok, ' + res.message.user.name + ', jeg skal prøve å huske: ' + husk + '.');
   });
 
   robot.respond(/huskeliste/i, function (res) {
-    res.send('Sorry ass. Jeg har glemt det alt.');
+    var husk = robot.brain.get('husk');
+    if (husk) {
+      res.set(husk);
+    }
+    else
+      res.send('Sorry ass. Jeg har glemt det alt.');
   });
 }
