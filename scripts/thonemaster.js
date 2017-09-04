17e812d2aebde05b70c04dd3c954a948d6cf9354
Petter Hareim <petter.hareim@bouvet.no>
2017-09-04 19:04:21 +0200

    better status-message (last move).

--- scripts/thonemaster.js
+++ scripts/thonemaster.js
@@ -5,8 +5,26 @@ var id = 131993;
 module.exports = function (robot) {
   robot.respond(/status/i, function (res) {
     thronemaster.getLog(id, function (status) {
-      var                     t = status.pop();
-      res.send('Siste flytt i runde ' + t.turn + ' er: ' + t.player + ' ' + t.logEntry);
+      if (!status || status.constructor !== Array) {
+        res.send('Sorry ass. Fant ikke det du lette etter.');
+        return;
+      }
+      var t = status[status.length - 1];
+      //res.send('Siste flytt i runde ' + t.turn + ' er: ' + t.player + ' ' + t.logEntry);
+      res.send({
+        "attachments": [
+          {
+            "title": "Runde " + t.turn + ", fase " + t.gamePhase + ".",
+            "title_link": "http://game.thronemaster.net/?game=131993"
+          },
+          {
+            "author_name": t.player,
+            // "title": "Move"+t.turn,
+            "text": t.logEntry,
+            "footer": "Move" + t.turn + ": " + t.date
+          }
+        ]
+      });
     });
   });
 };
